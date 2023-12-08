669e5a8493ad974310e9e120e0150f7ead8ef586
Edgar Parucho <parucho.edgar@outlook.es>
2023-12-07 20:48:52 -0400

    Refactoring query's filters normalization

--- src/utils/normalizeQuery.js
+++ src/utils/normalizeQuery.js
@@ -1,20 +1,35 @@
 const { Op } = require('sequelize');
 
 module.exports = function normalizeQueryFilters(filters) {
-  if (filters.fromDate !== undefined && filters.toDate !== undefined) {
-    filters.date = { [Op.between]: [filters.fromDate, filters.toDate] }
-    delete filters.fromDate;
-    delete filters.toDate;
-  } else if (filters.fromDate !== undefined) {
-    filters.date = { [Op.gte ]: filters.fromDate }
-    delete filters.fromDate;
-  } else if (filters.toDate !== undefined) {
-    filters.date = { [Op.lte ]: filters.toDate }
-    delete filters.toDate;
-  }
-  if (filters.note !== undefined) filters.note = { [Op.like]: "%" + filters.note + "%" };
-  if (filters.fundID !== undefined) {
-    filters[Op.or] = [{ fundID: filters.fundID }, { otherFundID: filters.fundID }]
-    delete filters.fundID;
-  }
+  if (filters.fromDate !== undefined && filters.toDate !== undefined) setDateRange(filters);
+  else if (filters.fromDate !== undefined) setDateFrom(filters);
+  else if (filters.toDate !== undefined) setDateTo(filters);
+  if (filters.note !== undefined) useLikeOperator(filters);
+  if (filters.fundID !== undefined) setBothFundsFilter(filters);
+  console.log(filters);
+}
+
+function setDateRange(filters) {
+  filters.date = { [Op.between]: [filters.fromDate, filters.toDate] }
+  delete filters.fromDate;
+  delete filters.toDate;
+}
+
+function setDateFrom(filters) {
+  filters.date = { [Op.gte ]: filters.fromDate }
+  delete filters.fromDate;
+}
+
+function setDateTo(filters) {
+  filters.date = { [Op.lte ]: filters.toDate }
+  delete filters.toDate;
+}
+
+function useLikeOperator(filters) {
+  filters.note = { [Op.like]: "%" + filters.note + "%" };
+}
+
+function setBothFundsFilter(filters) {
+  filters[Op.or] = [{ fundID: filters.fundID }, { otherFundID: filters.fundID }]
+  delete filters.fundID;
 }
